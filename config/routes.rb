e1acc84f7a8c5fa913723c55bb1ed4341d32676f
jackymeslin <jackymeslin@hotmail.com>
2020-04-14 14:38:20 +0200

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,22 @@
 Rails.application.routes.draw do
+
   devise_for :users
   root to: 'pages#home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+
+
+  resources :courses, only: [:index, :show] do
+    resources :steps, only: :show
+    resources :user_courses, only: :create
+  end
+
+
+  resources :user_steps, only: [:update] do
+    member do
+      patch :mark_as_done
+    end
+  end
+
+  resource :profile, only: :show
+
 end
