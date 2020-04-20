434ba9ce59f95fa8f77de1d46668ba3fbc0224b8
mathieu rimbault <rimbaultandco@gmail.com>
2020-04-20 14:09:38 +0200

    home with loading logo

--- app/javascript/components/animate_home.js
+++ app/javascript/components/animate_home.js
@@ -2,6 +2,8 @@ const animateHome =
 
 $(document).ready(function(){
             $(".animate").css('display', 'block').delay( 2000 ).fadeOut( 500 );
+            $(".testload").css('display', 'block').delay( 2000 ).fadeOut( 500 );
+
     jQuery.dequeue( this )
     });
 
