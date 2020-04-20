a282d82688324d1788448e4e0c6c8d3bdc25b3d4
Leo Gou <leo_g92@hotmail.fr>
2020-04-20 18:05:23 +0200

    Search fixed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,7 +16,7 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-
+import 'mapbox-gl/dist/mapbox-gl.css';
 // ----------------------------------------------------
 // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
 // WRITE YOUR OWN JS STARTING FROM HERE 👇
