fcf35acfedf5ad88af11289159de082a51954a73
jackymeslin <jackymeslin@hotmail.com>
2020-04-21 13:23:37 +0200

    button submit update done & answer OK

--- app/controllers/user_steps_controller.rb
+++ app/controllers/user_steps_controller.rb
@@ -3,22 +3,22 @@ class UserStepsController < ApplicationController
   def update
     @user_step = UserStep.find(params[:id])
     if @user_step.update(user_step_params)
+      @user_step.done = true
+      @user_step.save
+
+      last_user_step = @user_step.user_course.user_steps.joins(:step).order("steps.position").last
+      if @user_step == last_user_step
+        @user_step.user_course.update(done: true)
+      end
       redirect_to course_path(@user_step.step.course)
-      flash[:notice] = "Votre étape a bien été validé"
+      flash[:notice] = "Votre étape a bien été validée"
     else
       render 'steps/show'
     end
   end
 
   def mark_as_done
-    @user_step = UserStep.find(params[:id])
-    @user_step.done = true
-    @user_step.save
 
-    last_user_step = @user_step.user_course.user_steps.order(:position).last
-    if @user_step == last_user_step
-      @user_step.user_course.update(done: true)
-    end
     # if user_step.step.course.done == false
     #   user_step.step.course.done == true
     # else
