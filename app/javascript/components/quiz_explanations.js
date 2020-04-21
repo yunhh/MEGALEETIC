b2eef913037a34812e478f3ac3dc898508586612
jackymeslin <jackymeslin@hotmail.com>
2020-04-21 16:59:36 +0200

    JS OK THX Lomig

--- app/javascript/components/quiz_explanations.js
+++ app/javascript/components/quiz_explanations.js
@@ -9,12 +9,31 @@ const quizExplanations = () => {
   const btn = document.getElementById('toggle-answer');
   const explain = document.getElementById('explanations');
   const sbmt = document.getElementById('submit-quiz');
+  const rightAnswerField = document.querySelector('.right-answer');
+  const radioButtons = document.querySelectorAll('.radio_buttons')
   if (btn){
     btn.addEventListener('click', (event) => {
       event.preventDefault();
       explain.style.display = "block";
       btn.style.display = "none";
+
       sbmt.classList.remove("hidden");
+
+      const rightAnswer = rightAnswerField.dataset.answer;
+
+      let userAnswer = "";
+      radioButtons.forEach((button) => {
+        if (button.checked){
+          userAnswer = button.value;
+        };
+      });
+
+      if (rightAnswer === userAnswer){
+        rightAnswerField.insertAdjacentHTML("beforeend", "Félicitations c'est la bonne réponse !");
+      } else {
+        rightAnswerField.insertAdjacentHTML("beforeend", `Hélas la bonne réponse était ${rightAnswer}...`);
+      }
+
     });
   }
 }
