94cb7817b66eed66a40dd8958f8abc1ec309e367
guillaume Demasse demasseguillaume@gmail.com <demasseguillaume@gmail.com>
2020-04-21 11:10:56 +0200

    mark_as_done fait

--- app/controllers/user_steps_controller.rb
+++ app/controllers/user_steps_controller.rb
@@ -4,10 +4,25 @@ class UserStepsController < ApplicationController
     @user_step = UserStep.find(params[:id])
     if @user_step.update(user_step_params)
       redirect_to course_path(@user_step.step.course)
-      flash[:notice] = "Merci d'avoir répondu"
+      flash[:notice] = "Votre étape a bien été validé"
     else
       render 'steps/show'
     end
+  end
+
+  def mark_as_done
+    @user_step = UserStep.find(params[:id])
+    @user_step.done = true
+    @user_step.save
+
+    last_user_step = @user_step.user_course.user_steps.order(:position).last
+    if @user_step == last_user_step
+      @user_step.user_course.update(done: true)
+    end
+    # if user_step.step.course.done == false
+    #   user_step.step.course.done == true
+    # else
+    redirect_to course_path(@user_step.step.course)
   end
 
   private
