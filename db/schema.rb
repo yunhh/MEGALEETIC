8c6b57567fe9da01769a1473a10522a04c999374
jackymeslin <jackymeslin@hotmail.com>
2020-04-24 11:47:22 +0200

    step distance_in_km integer to float

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_20_181515) do
+ActiveRecord::Schema.define(version: 2020_04_24_093449) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -82,7 +82,7 @@ ActiveRecord::Schema.define(version: 2020_04_20_181515) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "access_description"
-    t.integer "distance_in_kilometers"
+    t.float "distance_in_kilometers"
     t.integer "duration_in_minutes"
     t.string "explanations"
     t.index ["course_id"], name: "index_steps_on_course_id"
